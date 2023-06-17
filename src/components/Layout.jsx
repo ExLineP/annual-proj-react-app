dc8b37d1c14227e561fd9a761d9986c7f5fc51a6
ExLineP <Danilaanis@gmail.com>
2023-06-17 17:03:56 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/components/Layout.jsx
@@ -0,0 +1,16 @@
+﻿import React from "react";
+import {Outlet} from "react-router-dom";
+import Navbar1 from "./Navbar";
+
+const Layout = () => {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen overflow-hidden bg-gradient-to-b from-gray-700 via-sky-400 to-white">
+      <Navbar1 />
+      <div className="flex-grow overflow-auto">
+        <Outlet />
+      </div>
+    </div>
+  );
+};
+
+export default Layout;
---
+++ src/pages/graphspage.jsx
@@ -0,0 +1,10 @@
+﻿import Graphs from "../components/graphs.jsx";
+const GraphsPage = () => {
+  return (
+    <div className="mt-24">
+      <a className="text-4xl text-white text-center">Основные показатели Датасета обучения модели</a>
+      <Graphs />
+    </div>
+  );
+};
+export default GraphsPage;
